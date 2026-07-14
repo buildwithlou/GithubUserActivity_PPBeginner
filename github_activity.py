61f523020a3752eec7e69b993558703777558031
buildwithlou <buildwithlou@gmail.com>
2026-07-14 19:11:53 -0400

    Updates

--- github_activity.py
+++ github_activity.py
@@ -6,3 +6,12 @@ def main():
     # By checking the length of sys.argv is less than 2
     if len(sys.argv) < 2:
         print("Usage: python github_activity.py <username>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+
+    print(f"Debug: Successfully captured username: {username}")
+
+
+if __name__ == "__main__":
+    main()
